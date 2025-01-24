cdd67e6d9fa6fffb49555a14ddfe30a60dba1dff
AarzooDwivedi <125575396+AarzooDwivedi@users.noreply.github.com>
2025-01-24 22:33:17 +0530

    Add files via upload

---
+++ task-manager-backend/config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = async () => {
+  try {
+    const mongoURI = process.env.MONGODB_URI;
+
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
